6f2d70a1c7d40cab13e288b5d76057f8e69fde82
nixtrim <omuaz01@gmail.com>
2025-04-16 23:39:18 +0300

    ns

--- flake.nix
+++ flake.nix
@@ -1,70 +1,46 @@
 {
-  description = "The Amazing Nixcalibur";
+  description = "A nixvim configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixvim.url = "github:nix-community/nixvim";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
   };
 
   outputs =
-    {
-      nixpkgs,
-      nixvim,
-      flake-parts,
-      pre-commit-hooks,
-      ...
-    }@inputs:
+    { nixvim, flake-parts, ... }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
-        "aarch64-linux"
         "x86_64-linux"
-        "aarch64-darwin"
+        "aarch64-linux"
         "x86_64-darwin"
+        "aarch64-darwin"
       ];
 
       perSystem =
-        {
-          system,
-          pkgs,
-          self',
-          lib,
-          ...
-        }:
+        { system, ... }:
         let
           nixvimLib = nixvim.lib.${system};
           nixvim' = nixvim.legacyPackages.${system};
           nixvimModule = {
-            inherit pkgs;
+            inherit system; # or alternatively, set `pkgs`
             module = import ./config; # import the module directly
             # You can use `extraSpecialArgs` to pass additional arguments to your module files
             extraSpecialArgs = {
               # inherit (inputs) foo;
             };
           };
-          nixcalibur = nixvim'.makeNixvimWithModule nixvimModule;
+          nvim = nixvim'.makeNixvimWithModule nixvimModule;
         in
         {
           checks = {
+            # Run `nix flake check .` to verify that your config is not broken
             default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
-            pre-commit-check = pre-commit-hooks.lib.${system}.run {
-              src = ./.;
-              hooks = {
-                statix.enable = true;
-                nixfmt-rfc-style.enable = true;
-              };
-            };
           };
-
-          formatter = pkgs.nixfmt-rfc-style;
 
           packages = {
-            default = nixcalibur;
-          };
-
-          devShells = {
-            default = with pkgs; mkShell { inherit (self'.checks.pre-commit-check) shellHook; };
+            # Lets you run `nix run .` to start nixvim
+            default = nvim;
           };
         };
     };
